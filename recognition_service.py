71a8e69c520c2d1b831c95dffed046decf088c11
Changwoo Song <song921216@gmail.com>
2022-06-28 18:33:23 +0900

    Fixed things : E302, W293, E203

--- recognition_service.py
+++ recognition_service.py
@@ -1,15 +1,16 @@
 import boto3
 
+
 class RecognitionService:
     def __init__(self) -> None:
         self.client = boto3.client('rekognition')
-    
+
     def detect_objects(self, storage_location, image_file):
         response = self.client.detect_labels(
             Image = {
-                'S3Object' : {
-                    'Bucket' : storage_location,
-                    'Name' : image_file
+                'S3Object': {
+                    'Bucket': storage_location,
+                    'Name': image_file
                 }
             }
         )
